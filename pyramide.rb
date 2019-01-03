9e6aea93d338ddb7c9eb03ea4375ddff8df6921a
mad221 <maximepouzol@gmail.com>
2019-01-03 18:26:40 +0100

    oublie de push pyrammide op

--- pyramide.rb
+++ pyramide.rb
@@ -2,11 +2,17 @@ puts "veuillez saisir un nombre "
 
 nombre= gets.chomp.to_i
 i=0
+f=0
+space=" "
+g=nombre
 char="#"
+puts "voici la pyrammide "
  if(nombre<=25 &&  nombre>=1)
 while (nombre!=i)
 nombre=nombre-1
-puts char*nombre
+f=f+1
+g=g/2
+puts space*g+char*f
 
 end 
 end
